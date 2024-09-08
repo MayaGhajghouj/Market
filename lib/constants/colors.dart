d6d3dfbb4c254c9668a2aae623667f6375a3aab2
MayaGhajghouj <mmmmaaaayy33@gmail.com>
2024-09-08 22:12:54 +0300

    add colors to app

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color Black = Color(0xFF363130);
+const Color Mywieght = Color(0xffffffff);
+const Color salmon = Color(0xffF4B5A4);
+const Color Terracotta = Color(0xffCC7861);
+const Color brown_rosy = Color(0xffDCBEB6);
+const Color Beige = Color(0xffFAF0E6);
+
+
+
+
