e7aff18fbc810458ac16d03d8493f11bd9c48013
MayaGhajghouj <mmmmaaaayy33@gmail.com>
2024-09-16 19:59:08 +0300

    start welcome back page

---
+++ lib/pages/welcome_back_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WelcomeBackPage extends StatelessWidget {
+  const WelcomeBackPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
