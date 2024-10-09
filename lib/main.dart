329f5cd6449f47225b8352c7ae73449162a7a1b2
MayaGhajghouj <mmmmaaaayy33@gmail.com>
2024-10-09 20:10:59 +0300

    change the architecture of prj,and make a routers pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mmarket_interfaces/views/pages/dining_room/dining-table_home_page.dart';
-import 'package:mmarket_interfaces/views/pages/dining_room/product_description_DiningTable.dart';
-import 'package:mmarket_interfaces/views/pages/living_room/product_description_sofa.dart';
-import 'package:mmarket_interfaces/views/pages/living_room/sofa_home_page.dart';
-import 'package:mmarket_interfaces/views/pages/profile_pages/edit_profile.dart';
-import 'package:mmarket_interfaces/views/pages/profile_pages/my_orders.dart';
-import 'package:mmarket_interfaces/views/pages/profile_pages/profile.dart';
-import 'package:mmarket_interfaces/views/pages/review_page.dart';
-import 'package:mmarket_interfaces/views/pages/sections_livingroom.dart';
-import 'package:mmarket_interfaces/views/pages/welcome_back_page.dart';
-
+import 'package:mmarket_interfaces/core/app_routers.dart';
+import 'package:mmarket_interfaces/modules/dining_room/views/dining-table_home_page.dart';
+import 'package:mmarket_interfaces/modules/dining_room/views/product_description_DiningTable.dart';
+import 'package:mmarket_interfaces/modules/living_room/views/product_description_sofa.dart';
+import 'package:mmarket_interfaces/modules/living_room/views/sofa_home_page.dart';
+import 'package:mmarket_interfaces/modules/profile_pages/views/edit_profile.dart';
+import 'package:mmarket_interfaces/modules/profile_pages/views/my_orders.dart';
+import 'package:mmarket_interfaces/modules/profile_pages/views/profile.dart';
+import 'package:mmarket_interfaces/modules/reviewpage/views/review_page.dart';
+import 'package:mmarket_interfaces/modules/living_room/views/sections_livingroom.dart';
+import 'package:mmarket_interfaces/modules/welcomebackpage/views/welcome_back_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,19 +28,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute:  '/MyOrders',
-      getPages: [
-        GetPage(name: '/ProductDes_DiningTable', page: () => ProductDes_DiningTable()),
-        GetPage(name: '/DiningTableHomePage', page: () => DiningTableHomePage()),
-        GetPage(name: '/ProductDes_Sofa', page: () => ProductDes_Sofa()),
-        GetPage(name: '/SofaHomePage', page: () => SofaHomePage()),
-        GetPage(name: '/EditProfile', page: () => EditProfile()),
-        GetPage(name: '/MyOrders', page: () => MyOrders()),
-        GetPage(name: '/Profile', page: () => Profile()),
-        GetPage(name: '/ReviewPage', page: () => ReviewPage()),
-        GetPage(name: '/SectionsLivingroom', page: () => SectionsLivingroom()),
-        GetPage(name: '/WelcomeBackPage', page: () => WelcomeBackPage()),
-      ],
+      initialRoute: Routes.WelcomeBackPage,
+      getPages: AppPages.Pages,
     );
   }
 }
