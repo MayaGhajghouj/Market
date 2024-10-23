81072ad67b432b4b3feb0492f51d48e5f7a61cd5
MayaGhajghouj <mmmmaaaayy33@gmail.com>
2024-10-23 04:41:48 +0300

    add user model ,add google signin plugin,

---
+++ lib/models/user_model.dart
@@ -0,0 +1,44 @@
+class UserModel {
+  String? id;
+  String displayName;
+  String email;
+  String phone;
+  String birthday;
+  String? photoUrl;
+  String password;
+
+  UserModel({
+    this.id,
+    required this.displayName,
+    required this.email,
+    required this.phone,
+    required this.birthday,
+    this.photoUrl,
+    required this.password,
+  });
+
+  // Convert a UserModel instance to a Map for Firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'displayName': displayName,
+      'email': email,
+      'phone': phone,
+      'birthday': birthday,
+      'photoUrl': photoUrl,
+      'password': password,
+    };
+  }
+
+  // Convert a Map to a UserModel instance
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'],
+      displayName: map['displayName'],
+      email: map['email'],
+      phone: map['phone'],
+      birthday: map['birthday'],
+      photoUrl: map['photoUrl'],
+      password: map['password'],
+    );
+  }
+}
--- lib/modules/login_signin_page/widget_componants/show_alert_dialog.dart
+++ lib/modules/login_signin_page/widget_componants/show_alert_dialog.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mmarket_interfaces/core/app_routers.dart';
 import 'package:mmarket_interfaces/core/manage_app_state/app_state_controller.dart';
 
 void showEmailNotReceivedDialog(
@@ -11,19 +10,19 @@ void showEmailNotReceivedDialog(
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
-        title: Text('Email Not Verified'),
-        content: Text(
+        title: const Text('Email Not Verified'),
+        content: const Text(
             'It seems you have not received the email. Would you like to resend it?'),
         actions: [
           TextButton(
-            child: Text('Cancel'),
+            child: const Text('Cancel'),
             onPressed: () {
               Get.back();
               appStateController.setSuccess();
             },
           ),
           TextButton(
-            child: Text('Resend Email'),
+            child: const Text('Resend Email'),
             onPressed: () async {
               appStateController.startLoading();
               // Call your function to resend the email here
